35465867598d5c2a5c19a1ac3912bbf40e48d0a8
grigoriank8 <karogrigorian8@gmail.com>
2024-08-10 12:25:06 +0400

    frontend(header & landing pages)

--- frontend/src/header/Header.tsx
+++ frontend/src/header/Header.tsx
@@ -1,29 +1,31 @@
-import { Avatar } from '@mantine/core'
+import { Avatar, Indicator } from '@mantine/core'
 import { IconBell, IconBow, IconSettings } from '@tabler/icons-react'
+import NavLinks from './NavLinks'
 
 
 const Header = () => {
-  return <div className='w-full bg-black px-16 h-28 text-white flex justify-between items-center'>
+  return <div className='w-full bg-mine-shaft-950 px-16 h-20 text-white flex justify-between items-center'>
     
-    <div className='flex gap-3 items-center'>
-      <IconBow className='h-10 w-10' stroke={1.25}/>
+    <div className='flex gap-2 items-center text-bright-sun-400'>
+      <IconBow className='h-10 w-10' stroke={2.1}/>
       <div className='text-2xl font-semibold'>jobHunter</div>
     </div>
     
-    <div className='flex gap-3'>
-      <a href="">Find Jobs</a>
-      <a href="">Find Talent</a>
-      <a href="">Upload Job</a>
-      <a href="">About us</a>
-    </div>
+    {NavLinks()}
     
-    <div className='flex gap-5 items-center'>
+    <div className='flex gap-3 items-center'>
       <div className='flex gap-2 items-center'>
         <div>Name</div>
-        <Avatar src="" alt=''/>
+        <Avatar src="/img/avatar.png" alt=''/>
       </div>
-      <IconBell />
-      <IconSettings />
+      <div className='bg-mine-shaft-900 p-1.5 rounded-full'>
+        <IconSettings stroke={1.5}  />
+      </div>
+      <div className='bg-mine-shaft-900 p-1.5 rounded-full'>
+        <Indicator color="bright-sun.4" processing>
+          <IconBell stroke={1.5} />
+        </Indicator>
+      </div>
     </div>
   </div>
 }
--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,8 +1,12 @@
 import Header from '../header/Header'
+import DreamJob from '../landingPage/DreamJob'
 
 const HomePage = () => {
   return (
-    <Header />
+    <div className='min-h-[100vh] bg-mine-shaft-950 font-["poppins"]'>
+      <Header />
+      <DreamJob />
+    </div>
   )
 }
 
