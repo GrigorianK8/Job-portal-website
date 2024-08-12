d8546b000d19411586d83055e69ffc0af7df155f
grigoriank8 <karogrigorian8@gmail.com>
2024-08-12 17:52:27 +0400

    frontend(landing page & footer)

--- frontend/src/data/Data.tsx
+++ frontend/src/data/Data.tsx
@@ -1,6 +1,6 @@
-export const companies = ['Google', 'Amazon', 'Oracle', 'Pinterest', 'Walmart', 'Slack', 'Spotify', 'Figma', 'Netflix'];
+const companies = ['Google', 'Amazon', 'Oracle', 'Pinterest', 'Walmart', 'Slack', 'Spotify', 'Figma', 'Netflix'];
 
-export const jobCategory = [
+const jobCategory = [
   {
     name: "Arts & Design",
     desc: "Create and conceptualize artistic and design works.",
@@ -18,7 +18,7 @@ export const jobCategory = [
   },
   {
     name: "Digital Marketing",
-    desc: "Promote products and services using digital channels.",
+    desc: "Promote products and services.",
     jobs: 200
   },
   {
@@ -56,4 +56,35 @@ const work = [
     "name": "Get Hired",
     "desc": "Connect with employers and start your new job."
   }
+];
+
+const testimonials = [
+  {
+    "name": "Jane Rata",
+    "testimonal": "This job portal made job search easy and quick. Recommended to all job seekers!",
+    "rating": 4.5
+  },
+  {
+    "name": "John Doe",
+    "testimonal": "This platform is a game-changer! It connected me with my dream job in just a few days. Highly recommended!",
+    "rating": 4
+  },
+  {
+    "name": "Anna Smirnova",
+    "testimonal": "I've tried several job portals, but this one stands out. The user interface is intuitive, and the job matches are spot on!",
+    "rating": 5
+  },
+  {
+    "name": "Michael Lee",
+    "testimonal": "An excellent resource for job seekers! I found multiple opportunities that aligned perfectly with my skills and experience.",
+    "rating": 4.5
+  }
+];
+
+const footerLinks = [
+  { title: "Product", links: ["Find Job", "Find Company", "Find Employee"] },
+  { title: "Company", links: ["About Us", "Contact Us", "Privacy Policy", "Terms & Conditions"] },
+  { title: "Support", links: ["Help & Support", "Feedback", "FAQs"] }
 ]
+
+export { companies, footerLinks, jobCategory, testimonials, work }
